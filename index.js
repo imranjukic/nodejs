80aac734a25af6d4a47ee2c0271681384970e19b
Imran Jukic <imran.jukic@gmail.com>
2020-07-20 21:34:07 +0200

    novo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('expres');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/',(req,res,next)=>{
+    console.log("New visit from: "+req.ip);
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello world.');
+});
+
+app.listen(port,()=>{
+    console.log('Server listening on port'+port);
+});
